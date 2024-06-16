cf3583700d980de0645765bfb4c14d8102f21657
TheCodeSherpa <xo5oso2n@duck.com>
2024-06-16 22:54:23 +0800

    Minor changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="versacorellmchatapi",
-    version="0.1.0",
+    version="0.1.1",
     description="A Python library for interacting with the VersaCore LLM Chat API.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
